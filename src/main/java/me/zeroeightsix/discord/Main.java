69c10fcf2f31a0e53b07413c5691d873af3f9f7c
EmotionalLove <sashwho@vivaldi.net>
2019-04-24 11:12:07 -0700

    register cmds

--- src/main/java/me/zeroeightsix/discord/Main.java
+++ src/main/java/me/zeroeightsix/discord/Main.java
@@ -1,5 +1,6 @@
 package me.zeroeightsix.discord;
 
+import me.zeroeightsix.discord.command.*;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
@@ -15,11 +16,18 @@ public class Main {
 
     public static JDA jda;
 
-    public static void main(String[] args) throws LoginException, InterruptedException {
+    public static void main(String[] args) throws LoginException, InterruptedException, InstantiationException, IllegalAccessException {
         if (args.length == 0) {
             System.err.println("Discord token required as argument");
             return;
         }
+        MapArtBot.commandProcessor.register(AddCommand.class);
+        MapArtBot.commandProcessor.register(FagCommand.class);
+        MapArtBot.commandProcessor.register(HelpCommand.class);
+        MapArtBot.commandProcessor.register(MapCommand.class);
+        MapArtBot.commandProcessor.register(OpenCommand.class);
+        MapArtBot.commandProcessor.register(RemoveCommand.class);
+        MapArtBot.commandProcessor.register(ResetCommand.class);
         JDABuilder builder = new JDABuilder(AccountType.BOT);
         jda = builder.setAutoReconnect(true).setToken(args[0]).buildBlocking();
         File tmp = new File("tmp");
